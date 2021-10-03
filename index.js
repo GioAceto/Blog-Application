db66a1e952626f17c5558cc71f54b4500ede70b9
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-10-03 14:42:16 -0400

    change file extensions and routes to ejs

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ app.set('view engine', 'ejs');
 PORT = 4000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+  res.render('index')
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+  res.render('about')
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+  res.render('contact')
 });
 
 app.get('/post', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+  res.render('post')
 });
 
 app.listen(PORT, () => {
