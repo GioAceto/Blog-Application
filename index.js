0ed06469118d7f94f22282807a3036d2533b3cc9
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-10-03 10:48:43 -0400

    add get route for home page

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
 
 PORT = 4000;
 
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+});
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
-})
+});
